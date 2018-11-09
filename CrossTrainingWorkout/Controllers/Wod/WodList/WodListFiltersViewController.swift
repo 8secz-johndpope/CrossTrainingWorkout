28a3e5707487e8a2b124054d3aff53f7ff7ddde8
Jonathan Arnal <jarnal@nouveal.com>
2018-11-09 11:44:47 +0100

    wod list files structure done

--- CrossTrainingWorkout/Controllers/Wod/WodList/WodListFiltersViewController.swift
+++ CrossTrainingWorkout/Controllers/Wod/WodList/WodListFiltersViewController.swift
@@ -1,13 +1,12 @@
 //
-//  WodListViewController.swift
+//  WodListFiltersViewController.swift
 //  CrossTrainingWorkout
 //
-//  Created by Jonathan Arnal on 08/11/2018.
+//  Created by Jonathan Arnal on 09/11/2018.
 //  Copyright © 2018 Nouveal. All rights reserved.
 //
 
 import UIKit
-import SnapKit
 
 class WodListFiltersViewController: UIViewController {
     
@@ -22,26 +21,6 @@ class WodListFiltersViewController: UIViewController {
         didSet {
             headerTabBar.update(withElements: tabBarElements)
         }
-    }
-    
-}
-
-class WodListTableViewController: UIViewController {
-    
-    
-    
-}
-
-class WodListViewController: UIViewController, NibLoadable {
-    
-    override func viewDidLoad() {
-        
-//        let xibView = WodListView(frame: self.view.frame)
-//        xibView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(xibView)
-//        xibView.snp.makeConstraints { (make) in
-//            make.edges.equalToSuperview()
-//        }
     }
     
 }
---
+++ CrossTrainingWorkout/Controllers/Wod/WodList/WodListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WodListViewController.swift
+//  CrossTrainingWorkout
+//
+//  Created by Jonathan Arnal on 08/11/2018.
+//  Copyright © 2018 Nouveal. All rights reserved.
+//
+
+import UIKit
+
+class WodListViewController: UIViewController, NibLoadable {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
